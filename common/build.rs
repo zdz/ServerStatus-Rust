ddd3ccb54e04a2ee2a31f4447db86b9293d55945
doge <doge.py@gmail.com>
2022-08-15 23:04:37 +0800

    Update Rust Crate

--- common/build.rs
+++ common/build.rs
@@ -35,6 +35,7 @@ fn main() {
     }
     println!("cargo:rustc-env=APP_VERSION={}", app_version);
 
+    std::env::set_var("PROTOC", protobuf_src::protoc());
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .compile(&["proto/server_status.proto"], &["proto"])
